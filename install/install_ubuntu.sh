e0250cc37bee523a175db0cb996438da2b378fa3
lyle <lilyui@126.com>
2018-04-10 16:47:49 +0800

    update

--- install/install_ubuntu.sh
+++ install/install_ubuntu.sh
@@ -27,11 +27,10 @@ cd yara
 make
 sudo make install
 
-sudo pip install --upgrade pip
 sudo pip install -U scikit-learn numpy scipy matplotlib
-sudo pip install -U magic
+sudo pip install -U magic pandas
 sudo pip3 install --user numpy scipy
-sudo pip3 install -U scikit-learn matplotlib
+sudo pip3 install -U scikit-learn matplotlib pandas
 
 git clone https://github.com/faizann24/Fwaf-Machine-Learning-driven-Web-Application-Firewall
 mv Fwaf-Machine-Learning-driven-Web-Application-Firewall Fwaf
